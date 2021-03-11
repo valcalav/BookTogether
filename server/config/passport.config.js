03e1f971e30c6d686c71b9b2f0c06b965b5a8e22
Veronica Alcala <veronica.alcala@gmail.com>
2021-03-11 20:40:49 +0100

    deploy

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -27,9 +27,9 @@ module.exports = app => {
             else next(new Error("no user"))
         })
         .catch(err => next(err))
+        
         // User
         //     .findById(id, (err, user) => {
-
         //         if (err) { 
         //             next(err); 
         //             return
@@ -52,28 +52,38 @@ module.exports = app => {
     app.use(flash())
 
     passport.use(new LocalStrategy({ passReqToCallback: true }, (req, username, password, next) => {
-    
-        User.findOne({username: username})
-            .then(user => {
-                if (user) {
-                    return next(null, user)
-                } else {
-                    return Reader
-                        .findOne({ "userInfo.username": username }, (err, user) => {
-                        if (err) {
-                            return next(err);
-                        }
-                        if (!user) {
-                            return next(null, false, { message: "Incorrect username" });
-                        }
-                        if (!bcrypt.compareSync( password, user.userInfo.password)) {
-                            return next(null, false, { message: "Incorrect password" });
-                        }
-            
-                        return next(null, user);
-                    })
-            }})
+        Promise.all([ User.findOne({username: username}), Reader
+            .findOne({ "userInfo.username": username })])
+            .then( res => {
+                if(res[0]) next(null, res[0])
+                else if(res[1]) next(null, res[1])
+                else {
+                    if(!bcrypt.compareSync( password, user.userInfo.password)) next(null, false, {message: "Incorrect password"})
+                    else next(null, false, {message: ":ncorrect username"})
+                }
+            })
             .catch(err => res.status(500).json(err))
+        // User.findOne({username: username})
+        //     .then(user => {
+        //         if (user) {
+        //             return next(null, user)
+        //         } else {
+        //             return Reader
+        //                 .findOne({ "userInfo.username": username }, (err, user) => {
+        //                 if (err) {
+        //                     return next(err);
+        //                 }
+        //                 if (!user) {
+        //                     return next(null, false, { message: "Incorrect username" });
+        //                 }
+        //                 if (!bcrypt.compareSync( password, user.userInfo.password)) {
+        //                     return next(null, false, { message: "Incorrect password" });
+        //                 }
+            
+        //                 return next(null, user);
+        //             })
+        //     }})
+        // .catch(err => res.status(500).json(err))
     }))
 
     app.use(passport.initialize())
